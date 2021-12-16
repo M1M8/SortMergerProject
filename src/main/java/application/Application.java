12368da7c68249f125cd383699371bcddcd851da
Michael <michael_makam18@hotmail.com>
2021-12-16 16:29:54 +0000

    Application class updated with a run() method.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,6 +1,40 @@
 package application;
 
+import display.Display;
+import user.User;
+
 public class Application {
 
-    private boolean isRunning = true;
+    private static User user = new User();
+
+    private static void choose(User.Choice choice) {
+        int intChoice = 0;
+
+        while(intChoice <= 0) {
+
+            intChoice = switch (choice) {
+                case SORTER -> user.setUserSelection(User.Choice.SORTER);
+                case ARRAY_LENGTH -> user.setUserSelection(User.Choice.ARRAY_LENGTH);
+            };
+
+            if(intChoice <= 0) {
+                System.out.println("Invalid input, please try again");
+            }
+        }
+
+    }
+
+    public static void run() {
+
+        Display.displayChoiceOfArrays();
+        choose(User.Choice.SORTER);
+
+        Display.displayChoiceOfArraySize();
+        choose(User.Choice.ARRAY_LENGTH);
+
+        Display.displayUserResults();
+
+    }
+
+
 }
