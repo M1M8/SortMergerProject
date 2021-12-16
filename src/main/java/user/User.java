b13f261103a52915766b36eb00537be7fa1e3f8c
Michael <michael_makam18@hotmail.com>
2021-12-16 08:05:49 +0000

    User class added to project. Responsible for storing user input data

---
+++ src/main/java/user/User.java
@@ -0,0 +1,23 @@
+package user;
+
+public class User {
+
+    private int sorterChoice;
+    private int choiceOfArrayLength;
+
+    public int getChoiceOfArrayLength() {
+        return choiceOfArrayLength;
+    }
+
+    public void setChoiceOfArrayLength(int choiceOfArrayLength) {
+        this.choiceOfArrayLength = choiceOfArrayLength;
+    }
+
+    public int getSorterChoice() {
+        return sorterChoice;
+    }
+
+    public void setSorterChoice(int sorterChoice) {
+        this.sorterChoice = sorterChoice;
+    }
+}
