bf7304eddebd90a9913dc4afca6763238fda066f
Michael <michael_makam18@hotmail.com>
2021-12-16 16:36:00 +0000

    User class updated with setUserSelection() to handle user choice.

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,9 +1,21 @@
 package user;
 
+import java.util.Scanner;
+
 public class User {
+
+    public enum Choice{
+        SORTER,
+        ARRAY_LENGTH
+    }
 
     private int sorterChoice;
     private int choiceOfArrayLength;
+
+    public User() {
+        this.sorterChoice = 0;
+        this.choiceOfArrayLength = 0;
+    }
 
     public int getChoiceOfArrayLength() {
         return choiceOfArrayLength;
@@ -20,4 +32,28 @@ public class User {
     public void setSorterChoice(int sorterChoice) {
         this.sorterChoice = sorterChoice;
     }
+
+    public int setUserSelection(Choice choice) {
+
+        int selection;
+
+        switch (choice) {
+            case SORTER:
+                selection = sorterChoice;
+                break;
+            case ARRAY_LENGTH:
+                selection = choiceOfArrayLength;
+                break;
+        }
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            selection = scanner.nextInt();
+        } catch (Exception isInvalidInput) {
+            return 0;
+        }
+
+        return selection;
+    }
+
 }
