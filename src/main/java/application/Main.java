ed3f90e5ad4c8e7c36765d46fda39d5de3ce8605
Michael <michael_makam18@hotmail.com>
2021-12-17 17:17:41 +0000

    Main class updated

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,7 +3,8 @@ package application;
 public class Main {
 
     public static void main(String[] args) {
-        Application.run();
+        Application app = new Application();
+        app.run();
     }
 
 }
