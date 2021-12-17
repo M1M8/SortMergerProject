2b06d73148939f12a3c927dd1f50dccf5b1aa7f4
Michael <michael_makam18@hotmail.com>
2021-12-17 17:22:32 +0000

    MergeSort class added to project.

---
+++ src/main/java/sorter/MergeSort.java
@@ -0,0 +1,19 @@
+package sorter;
+
+public class MergeSort extends Sorter {
+
+    public MergeSort() {
+        this.name = "MergeSort";
+    }
+
+
+    @Override
+    public int[] sort(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+}
