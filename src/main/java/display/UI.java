806898c1e67458ee29ed1037b8cb8e32b0805910
Michael <michael_makam18@hotmail.com>
2021-12-17 17:18:13 +0000

    Display class updated. Name changed to UI.

---
+++ src/main/java/display/UI.java
@@ -0,0 +1,40 @@
+package display;
+
+import arraygenerator.ArrayGenerator;
+import printer.Printer;
+import user.User;
+
+import java.util.Arrays;
+
+public class UI {
+
+    public UI() {
+    }
+
+    public void displayChoiceOfArrays() {
+        Printer.printMessage("Sort Manager: Sort an array using different sort algorithms");
+        Printer.printMessage("Pick a sort algorithm [Enter corresponding number]: ");
+        Printer.printMessage("1. BubbleSort");
+//        Printer.printMessage("2. BinaryTreeSort");
+//        Printer.printMessage("3. MergeSort");
+
+    }
+
+    public void displayChoiceOfArraySize() {
+        Printer.printMessage("Please enter the size of the array you wish to generate (max size 100):");
+    }
+
+    public void displayUserResults(int lengthOfArray, String unsortedArray, String sortedArray) {
+        Printer.printMessage("Sorting using the");
+
+        Printer.printMessage("Before sorting:");
+        Printer.printMessage("Length of Array: " + lengthOfArray);
+        Printer.printMessage(unsortedArray);
+
+        Printer.printMessage("After sorting:");
+        Printer.printMessage(sortedArray);
+
+        //"Time taken: nanoseconds"
+        Printer.printMessage("Time taken: ");
+    }
+}
