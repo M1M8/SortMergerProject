ae1d2c7b82c7e9cff28ba23bf6466557028b4e57
Michael <michael_makam18@hotmail.com>
2021-12-17 17:21:42 +0000

    BinaryTreeSort class added to project.

---
+++ src/main/java/sorter/BinaryTreeSort.java
@@ -0,0 +1,18 @@
+package sorter;
+
+public class BinaryTreeSort extends Sorter {
+
+    public BinaryTreeSort() {
+        this.name = "BinaryTreeSort";
+    }
+
+    @Override
+    public int[] sort(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+}
